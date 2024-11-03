2cecaeb1c7e3280f3291a24b02fd50f50290bb02
TrKNguyen <nguyenusaco@gmail.com>
2024-11-03 21:30:22 +0800

    Some note about raw pointer

--- RawPointers.cpp
+++ RawPointers.cpp
@@ -6,7 +6,21 @@ int main() {
     int* x = new int(20);
     cout << y << " " << *x <<endl;
     vector<int> vt = *(new vector<int>(20, 100));
-    for (int i = 0; i < 20; i++) {
+    for (int i = 0; i < 5; i++) {
         cout << vt[i] << " " << i <<endl;
     }
+    int* firstElement = (new int[25]);
+    for (int t = 0; t < 5; t++) {
+        if (t < 3) *(firstElement + t) = t + 10;
+        cout << firstElement + t <<" "<<*(firstElement + t) << endl;
+    }
+    delete firstElement;
+    cout <<"After delete" << endl; 
+    for (int t = 0; t < 5; t++) {
+        cout << firstElement + t <<" "<<*(firstElement + t) << endl;
+    }
+    // raw pointer is a normal pointer, using this one you must manually handle allocation and deallocation
+    // a memory leak when a program allocates memory but does not release it after the memory no longer needed. 
+    // => lead to unused memory that cannot be reclaimed, potentially causing the program to use more memory than necessary and in severe case, exhausting available memory 
+    
 }
