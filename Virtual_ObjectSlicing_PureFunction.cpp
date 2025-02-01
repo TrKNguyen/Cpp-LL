0e020438e7d56bbe67fc4ddc457be467756f0d74
TrKNguyen <nguyenusaco@gmail.com>
2025-02-01 14:22:09 +0800

    Add Virtual Table and Pure Function

--- Virtual_ObjectSlicing_PureFunction.cpp
+++ Virtual_ObjectSlicing_PureFunction.cpp
@@ -1,6 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std; 
-
+// VTable 
+/*
+A VTable is a lookup table storing pointers to virtual functions of a class.
+Each class with virtual functions has a single VTable.
+Each object of such a class has a hidden pointer (called the vptr) pointing to the class’s VTable.
+When a virtual function is called via a pointer/reference, the vptr is used to find the correct function at runtime.
+*/
 class Base {
 public:
     virtual void show() {  // Virtual enables dynamic binding
@@ -28,6 +34,18 @@ public:
 //         cout << "Derived class function\n";
 //     }
 // };
+
+class Animal {
+public:
+    virtual void makeSound() = 0; // Must be implemented by derived classes, pure function
+
+    void sleep() { cout << "Sleeping...\n"; } // Shared behavior
+};
+
+class Dog : public Animal {
+public:
+    void makeSound() override { cout << "Bark!\n"; }
+};
 
 int main() {
     Base* ptr;
