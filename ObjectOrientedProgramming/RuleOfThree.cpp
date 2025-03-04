6c0cfc6a571bd8ef894768ce9645578ac8c3e8c4
TrKNguyen <nguyenusaco@gmail.com>
2025-03-04 03:34:23 +0000

    Add rule of three

---
+++ ObjectOrientedProgramming/RuleOfThree.cpp
@@ -0,0 +1,13 @@
+/*
+    The famous rule of three
+
+It is a very well-known C++ principle say that if a class requires 
+a user-defined copy constructor, copy assignment and destructor, then
+it probably requires all three. In C++11, this was expanded to the rule 
+of fiveeeeeeeeeeeeeeee
+
+When we have move constructor and move assignment alsooo. 
+
+
+
+*/
