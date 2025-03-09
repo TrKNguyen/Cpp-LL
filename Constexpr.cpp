b54432fb8d71c694df65f7dac688a9e6c1a721c3
TrKNguyen <nguyenusaco@gmail.com>
2025-03-09 17:52:41 +0800

    Add best practice and comparison table for constexpre with other terms

--- Constexpr.cpp
+++ Constexpr.cpp
@@ -57,3 +57,32 @@ int main() {
     constexpr long long ans = runForLongTime(n);
     cout << ans << endl;
 }
+
+/*
+
+Best practice
+
+Any constant variable whose initializer is a constant expression should 
+be declared as constexpr.
+
+Any constant variable whose initializer is not a constant expression 
+(making it a runtime constant) should be declared as const
+
++------------------------+--------------------------------------------------------------------------------------+
+| Term                   | Definition                                                                          |
++------------------------+--------------------------------------------------------------------------------------+
+| Compile-time constant  | A value or non-modifiable object whose value must be known at compile time          |
+|                        | (e.g., literals and constexpr variables).                                           |
++------------------------+--------------------------------------------------------------------------------------+
+| Constexpr              | Keyword that declares objects as compile-time constants (and functions that can be  |
+|                        | evaluated at compile-time). Informally, shorthand for “constant expression.”        |
++------------------------+--------------------------------------------------------------------------------------+
+| Constant expression    | An expression that contains only compile-time constants and operators/functions      |
+|                        | that support compile-time evaluation.                                               |
++------------------------+--------------------------------------------------------------------------------------+
+| Runtime expression     | An expression that is not a constant expression.                                    |
++------------------------+--------------------------------------------------------------------------------------+
+| Runtime constant       | A value or non-modifiable object that is not a compile-time constant.               |
++------------------------+--------------------------------------------------------------------------------------+
+
+*/
